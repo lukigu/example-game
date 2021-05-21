84017d4422bde1b7bc3e103f2f0e24ae726e59e0
lukigu <82062009+lukigu@users.noreply.github.com>
2021-05-22 00:00:43 +0200

    moving player and following camera

---
+++ Example Game/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    // The target we are following
+    public Transform target;
+    // The distance in the x-z plane to the target
+    //So this would be your offset
+    private float distance = 3.8f;
+    // the height we want the camera to be above the target
+    private float height = 2.8f;
+    // How much we 
+    private float heightDamping = 2.0f;
+    private float rotationDamping = 3.0f;
+
+    // Place the script in the Camera-Control group in the component menu
+    [AddComponentMenu("Camera-Control/Smooth Follow")]
+
+    void LateUpdate()
+    {
+        // Early out if we don't have a target
+        if (!target) return;
+
+        // Calculate the current rotation angles
+        float wantedRotationAngle = target.eulerAngles.y;
+        float wantedHeight = target.position.y + height;
+
+        float currentRotationAngle = transform.eulerAngles.y;
+        float currentHeight = transform.position.y;
+
+        // Damp the rotation around the y-axis
+        currentRotationAngle = Mathf.LerpAngle(currentRotationAngle, wantedRotationAngle, rotationDamping * Time.deltaTime);
+
+        // Damp the height
+        currentHeight = Mathf.Lerp(currentHeight, wantedHeight, heightDamping * Time.deltaTime);
+
+        // Convert the angle into a rotation
+        var currentRotation = Quaternion.Euler(0, currentRotationAngle, 0);
+
+        // Set the position of the camera on the x-z plane to:
+        // distance meters behind the target
+        transform.position = target.position;
+        transform.position -= currentRotation * Vector3.forward * distance;
+
+        // Set the height of the camera
+        transform.position = new Vector3(transform.position.x, currentHeight, transform.position.z);
+
+        // Always look at the target
+        transform.LookAt(target);
+    }
+
+
+
+
+
+    ////public GameObject MainCharakter;
+    //private Vector3 offset = new Vector3(0, 3, -3);
+
+    ////////////////
+
+
+    //[SerializeField]
+    //private Transform target;
+
+    ////[SerializeField]
+    ////private Vector3 offsetPosition;
+
+    //[SerializeField]
+    //private Space offsetPositionSpace = Space.Self;
+
+    //[SerializeField]
+    //private bool lookAt = true;
+
+    //private void Update()
+    //{
+    //    Refresh();
+    //}
+
+    //public void Refresh()
+    //{
+    //    if (target == null)
+    //    {
+    //        Debug.LogWarning("Missing target ref !", this);
+
+    //        return;
+    //    }
+
+    //    // compute position
+    //    if (offsetPositionSpace == Space.Self)
+    //    {
+    //        transform.position = target.TransformPoint(offset);
+    //    }
+    //    else
+    //    {
+    //        transform.position = target.position + offset;
+    //    }
+
+    //    // compute rotation
+    //    if (lookAt)
+    //    {
+    //        transform.LookAt(target);
+    //    }
+    //    else
+    //    {
+    //        transform.rotation = target.rotation;
+    //    }
+    //}
+
+
+    //////////////
+
+    //// Use this for initialization
+    //void Start()
+    //{
+    //    //Calculate and store the offset value by getting the distance between the player's position and camera's position.
+    //    offset = transform.position - MainCharakter.transform.position;
+    //}
+
+    //// LateUpdate is called after Update each frame
+    //void LateUpdate()
+    //{
+    //    // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
+    //    transform.position = MainCharakter.transform.position + offset;
+    //}
+
+}
--- Example Game/Assets/Scripts/PlayerController.cs
+++ Example Game/Assets/Scripts/PlayerController.cs
@@ -2,39 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(CharacterController), typeof(Animator))]
 public class PlayerController : MonoBehaviour
 {
-    private CharacterController characterController;
-    private Animator animator;
 
-    [SerializeField]
-    private float movementSpeed, rotationSpeed, gravity;
-
-    private Vector3 movementDirection = Vector3.zero;
-    private bool playerGrounded;
-
+    public Animator anim;
+    private float speed = 5.0f;
+    private float turnSpeed = 150.0f;
+    // Move left right
+    private float horizontalInput;
+    // Move forward
+    private float verticallInput;
 
     // Start is called before the first frame update
     void Start()
     {
-        characterController = GetComponent<CharacterController>();
-        animator = GetComponent<Animator>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        playerGrounded = characterController.isGrounded;
-
-        //movement
-        Vector3 inputMovement = transform.forward * movementSpeed * Input.GetAxisRaw("Vertical");
-        characterController.Move(inputMovement * Time.deltaTime);
-
-        transform.Rotate(Vector3.up * Input.GetAxisRaw("Horizontal") * rotationSpeed);
-
-        //animations
-        animator.SetBool("isRunning", Input.GetAxisRaw("Vertical") != 0);
-
+        // Player input
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticallInput = Input.GetAxis("Vertical");
+        // Move forward
+        transform.Translate(Vector3.forward * Time.deltaTime * verticallInput * speed);
+        // Rotate car
+        transform.Rotate(Vector3.up, Time.deltaTime * horizontalInput * turnSpeed);
+        anim.SetFloat("vertical", Input.GetAxis("Vertical"));
     }
 }
